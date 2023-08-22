82e25b876e8987b7cadd0687c4078be8f893ee03
Leandro <franciscoleandrosilvacoutinho@gmail.com>
2023-08-21 22:00:28 -0300

    initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+   <h1>Hello world!</h1>
   );
 }
 
